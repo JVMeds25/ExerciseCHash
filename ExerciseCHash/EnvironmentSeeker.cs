f51c276da7ecb57804729ec1654c5fbb08c37aad
JV Medes <jvmedes01@gmail.com>
2024-02-18 10:44:12 +0800

    Add project files.

---
+++ ExerciseCHash/EnvironmentSeeker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseCHash
+{
+    class EnvironmentSeeker
+    {
+        public static void ShowEnvironmentDetails()
+        {
+            foreach (string drive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Drive: {0}", drive);
+            }
+
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of Processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".Net Version: {0}", Environment.Version);
+        }
+    }
+}
---
+++ ExerciseCHash/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciseCHash
+{
+    class Program
+    {
+        static int Main(string[] args) 
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("hello world");
+            //MessageBox.Show("All Done");
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine("Arg: {0}",args[i]);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No command lines");
+            }
+
+            //ShowEnvironmentDetails();
+            int[] valuesV = { 2, 7, 11, 15 };
+            int[] solvedV = TwoSum(valuesV, 9);
+
+            foreach(int i in solvedV)
+            {
+                Console.Write("{0}", solvedV[i]);
+            }
+            Console.ReadLine();
+            return 0;
+        }
+
+        public static void ShowEnvironmentDetails()
+        {
+            foreach (string drive in Environment.GetLogicalDrives())
+            {
+                //console.writeline("drive: {0}", drive);
+            }
+
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of Processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".Net Version: {0}", Environment.Version);
+            Console.WriteLine(" some where : {0}", Environment.Is64BitProcess);
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            LinkedList<int> answerVal = new LinkedList<int>();
+            int answerCount = 0;
+            int totalAnswer = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (target >= nums[i] && target > totalAnswer)
+                {
+                    
+                    totalAnswer += nums[i];
+                    if(totalAnswer <= target)
+                    {
+                        answerVal.AddLast(i);
+                    }
+                    answerCount++;
+                }
+            }
+            return answerVal.ToArray();
+        }
+    }
+}
+
